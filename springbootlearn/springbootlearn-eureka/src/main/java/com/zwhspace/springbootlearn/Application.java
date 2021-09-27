725c9ecf0c7f1fd9696ebd14debd97d4218f22e4
张文浩 <mrzhangemail@yeah.net>
2021-09-27 23:36:11 +0800

    结构调整

---
+++ springbootlearn/springbootlearn-eureka/src/main/java/com/zwhspace/springbootlearn/Application.java
@@ -0,0 +1,18 @@
+package com.zwhspace.springbootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Mr.Zhang
+ * @description 注册中心
+ * @date 2021/9/27 下午11:09
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
