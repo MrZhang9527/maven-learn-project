b8e6cc382bebd83d550e8884cadd948e3219305a
MrZhang <mrzhangemail@yeah.net>
2021-09-29 09:24:50 +0800

    日常练习

--- springbootlearn/springbootlearn-zuul/src/main/java/com/zwhspace/springbootlearn/Application.java
+++ springbootlearn/springbootlearn-zuul/src/main/java/com/zwhspace/springbootlearn/Application.java
@@ -2,6 +2,7 @@ package com.zwhspace.springbootlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * @date 2021/9/27 下午11:09
  */
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
---
+++ springbootlearn/springbootlearn-zuul/src/main/java/com/zwhspace/springbootlearn/IDepartmentController.java
@@ -0,0 +1,13 @@
+package com.zwhspace.springbootlearn;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@FeignClient(name = "service-department",path = "/department")
+public interface IDepartmentController {
+
+    @GetMapping("/getList")
+    Collection findDepartment();
+}
